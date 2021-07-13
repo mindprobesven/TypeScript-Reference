90cfd07ab9d559f5f29d18e5aadd1e7eda55d915
Sven Kohn <sven@mindprobe.io>
2021-07-13 15:34:49 +0200

    add: TypeScript reference

---
+++ 2021/4 - Something/dist/main.js
@@ -0,0 +1,33 @@
+"use strict";
+/*
+Chapter 1 - Setting up watch mode for the typescript compiler
+--------------------------------------------------------------------------------------
+This guide builds on [ 3 - Airbnb ESLint config with TypeScript support ]
+
+1. The tsconfig.json is updated to configure watch mode for the typescript compiler.
+Check file:
+./tsconfig.json
+
+In the package.json we add two new scripts:
+
+"build": "tsc",
+"build:watch": "tsc --watch"
+
+By running 'npm run build:watch', typescript now watches for any file changes and
+performs type-checking, as well as compiles to javascript into the ./dist directory.
+
+Chapter 2 - Running the compiled app with nodemon in watch mode
+--------------------------------------------------------------------------------------
+In the package.json we add two new scripts:
+
+"start:dev": "NODE_ENV=development nodemon dist/main.js",
+"start:prod": "NODE_ENV=production nodemon dist/main.js"
+
+Open a second terminal and run of the scritps. Nodemon will now watch for file changes
+in the ./dist directory, which happen after typescript finishes compiling and then
+restarts the compiled app.
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+const add_1 = require("./utils/add");
+console.log(`Running in ${process.env.NODE_ENV} mode`);
+console.log(add_1.default(5, 15));
---
+++ 2021/4 - Something/dist/utils/add.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const add = (a, b) => {
+    console.log('Adding...');
+    return a + b;
+};
+exports.default = add;
